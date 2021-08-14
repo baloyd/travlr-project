c4943b58dd26d80097ac44d987277320bc3adaa7
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-08-14 13:12:22 -0400

    Updated recommended

--- client/src/component/widgets/Recommended.js
+++ client/src/component/widgets/Recommended.js
@@ -15,17 +15,13 @@ const buttonStyle = {
     margin: '10px',
 }
 
-const sampleText= {
-    color: '#EF7E56',
-}
-
 const Resume=()=>{
     return(
         <CardColumns>
             <Card style={cardStyle} text='#305973'>
                 <Card.Body>
                     <Card.Title>Card Title</Card.Title>
-                    <Card.Subtitle className="mb-2"  style={sampleText}>Card Subtitle</Card.Subtitle>
+                    <Card.Subtitle className="mb-2">Card Subtitle</Card.Subtitle>
                     <Card.Text>
                     Some quick example text to build on the card title and make up the bulk of the card's content.
                     </Card.Text>
