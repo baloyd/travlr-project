1a11e577126ee7e9fbb7480c7dbe116ab457eb66
Vincent T <vyncode@gmail.com>
2021-08-13 21:22:01 -0400

    added models to mongoose

--- hello-react/server/models/index.js
+++ hello-react/server/models/index.js
@@ -1,3 +1,9 @@
-const User = require('./User');
+const Cata = require('./Cata')
+const City = require('./City')
+const Location = require('./Location')
+const Post = require('./Post')
+const Region = require('./Region')
+const State = require('./State')
+const User = require('./User')
 
-module.exports = { User };
+module.exports = { Cata, City, Location, Post, Region, State, User };
