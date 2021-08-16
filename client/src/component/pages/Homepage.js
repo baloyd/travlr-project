8ee80e3707aad1c42b2af20b2cd852cecc7c1c75
baloyd <danielbaloy@gmail.com>
2021-08-16 18:53:16 -0400

    navbar/bodycontainer

--- client/src/component/pages/Homepage.js
+++ client/src/component/pages/Homepage.js
@@ -1,7 +1,21 @@
-
+import { Jumbotron, Container, Button } from 'react-bootstrap';
+import Auth from '../utils/auth';
+import Login from '../Login'
+import Signup from '../Signup'
 
 const Homepage = ()=>{
+return (
+<div>
+<Jumbotron>
+    <Container>
+<h1>Travlr</h1>
 
+</Container>
+</Jumbotron>
+<Button><Login /></Button>
+<Button><Signup /></Button>
+</div>
+)
 }
 
 export default Homepage;
