de8a147868aa4cc66646147c311d1391cacc62bd
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-08-20 16:11:47 -0400

    Fixed layout of locationpage and added stylings, still missing to dynamically render each rec

--- client/src/component/widgets/CreateRec.js
+++ client/src/component/widgets/CreateRec.js
@@ -1,7 +1,6 @@
 import React, { useState } from  'react'
 import { Form, Row, Col, Button, Container } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
-import Modal from './Modal';
 
 // Stylings
 const formStyle = {
