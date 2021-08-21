94bd895a78fc592ea78def2bda8be6113b42c4b9
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-08-21 11:23:37 -0400

    Readded ids

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,19 @@
 import { gql } from '@apollo/client';
 
-
+export const QUERY_POST = gql`
+  query post($_id: String) {
+    post(_id: $_id) {
+      _id
+      name
+      street
+      city
+      state
+      zip
+      category
+      post_body
+    }
+  }
+`;
 
 
 
